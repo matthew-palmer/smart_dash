9d1022425c4d51cde7b0f44d8e1ae5f95a330870
Matt Palmer <mpalmer@coreinformatics.com>
2019-08-01 00:00:55 -0400

    [dev] Working changes for layout of project as well as functional Weather api using mock data.

---
+++ app/modules/weather/resources/Weather.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+
+API_TOKEN = ''
+
+
+class Weather:
+    def __init__(self, **kwargs):
+        global API_TOKEN
+        self.api_token = str(API_TOKEN)
+        self.base_url = kwargs['base_url'] if 'base_url' in kwargs else 'http://api.apixu.com'
+        self.version = kwargs['version'] if 'version' in kwargs else 'v1'
+        self.zipcode = kwargs['zipcode'] if 'zipcode' in kwargs else '06770'
+        self.is_mock = kwargs['is_mock'] if 'is_mock' in kwargs else False
+
+    def get_request_url(self, request_id):
+        url = "%s/%s/%s" % (self.base_url, self.version, request_id)
+        return url
+
+    def get_forecast_weather(self, days=7):
+        if self.is_mock:
+            with open('app/modules/weather/examples/forecast.json', 'r') as forecast:
+                return json.loads(forecast.read())
+        headers = {'content-type': 'application/json'}
+        params = {'key': self.api_token, 'days': days, 'q': self.zipcode}
+        response = requests.get(self.get_request_url(request_id='forecast.json'), params=params, headers=headers)
+        response.raise_for_status()
+        return json.loads(response.content)
+
+    def get_current_weather(self):
+        if self.is_mock:
+            with open('app/modules/weather/examples/current.json', 'r') as current:
+                return json.loads(current.read())
+        headers = {'content-type': 'application/json'}
+        params = {'key': self.api_token, 'q': self.zipcode}
+        response = requests.get(self.get_request_url(request_id='current.json'), params=params, headers=headers)
+        response.raise_for_status()
+        return json.loads(response.content)
+
+
+if __name__ == '__main__':
+    weather = Weather(is_mock=True)
+    print(weather.get_forecast_weather())
---
+++ app/modules/weather/routes.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify, current_app, render_template
+from app.resource.cache import cache
+
+from .api import *
+
+weather_blueprint = Blueprint('weather', __name__, url_prefix="/weather", template_folder='templates')
+
+
+@weather_blueprint.route('/')
+# @cache.cached(timeout=2592)
+def get_weather_current():
+    current_weather = get_current_weather(base_url=current_app.config['W_BASE_URL'],
+                                          version=current_app.config['W_VERSION'],
+                                          zipcode=current_app.config['W_ZIPCODE'],
+                                          is_mock=current_app.config['IS_MOCK'])
+    print(current_weather)
+    return render_template("current_weather.html", location=current_weather['location'], weather=current_weather['current'])
+
+
+@weather_blueprint.route('/forecast')
+# @cache.cached(timeout=2592)
+def get_weather_forecast():
+    forecast_weather = get_forecast_weather(7, base_url=current_app.config['W_BASE_URL'],
+                                            version=current_app.config['W_VERSION'],
+                                            zipcode=current_app.config['W_ZIPCODE'],
+                                            is_mock=current_app.config['IS_MOCK'])
+    print(forecast_weather)
+    return render_template("forecast.html", forecast=forecast_weather['forecast']['forecastday'],
+                           alerts=forecast_weather['alert'],
+                           location=forecast_weather['location'])
